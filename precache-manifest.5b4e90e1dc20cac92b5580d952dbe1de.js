4bd88fe704a61e10328e8e02dd7a300891d582d4
Binh Le <binhle@mit.edu>
2018-11-18 01:24:43 -0500

    Updates

---
+++ precache-manifest.5b4e90e1dc20cac92b5580d952dbe1de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae7c56c9758e042c36bf",
+    "url": "/LoL-Predict/static/js/runtime~main.ae7c56c9.js"
+  },
+  {
+    "revision": "6d5d911318634087e38a",
+    "url": "/LoL-Predict/static/js/main.6d5d9113.chunk.js"
+  },
+  {
+    "revision": "7e7d8b82aedefa3b8a55",
+    "url": "/LoL-Predict/static/js/1.7e7d8b82.chunk.js"
+  },
+  {
+    "revision": "6d5d911318634087e38a",
+    "url": "/LoL-Predict/static/css/main.dacd108a.chunk.css"
+  },
+  {
+    "revision": "3534fdbd1eaa542a31525e708e0f73e7",
+    "url": "/LoL-Predict/index.html"
+  }
+];
